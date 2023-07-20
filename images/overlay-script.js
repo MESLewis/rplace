df72814f6e3edb5eb061ffe861a377fbb40b5b94
Tobyn Nicholls <40596283+T-Nicholls@users.noreply.github.com>
2023-07-20 23:13:28 +0100

    Update overlay-script.js to work with r/place 2023

--- images/overlay-script.js
+++ images/overlay-script.js
@@ -4,13 +4,13 @@
 // @version      0.2
 // @description
 // @author       oralekin + odgrim + MESLewis
-// @match        https://hot-potato.reddit.com/embed*
+// @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].getElementsByTagName("garlic-bread-share-container")[0].getElementsByTagName("garlic-bread-camera")[0].getElementsByTagName("garlic-bread-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/MESLewis/rplace/main/images/combined-overlay.png";
