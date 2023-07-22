d47ecca0f3c1ddebd735b35c9dd365ee4e4df618
Tobyn Nicholls <40596283+T-Nicholls@users.noreply.github.com>
2023-07-22 16:10:16 +0100

    Update overlay-script.js to point to this repo.

--- images/overlay-script.js
+++ images/overlay-script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         NW Alliance Template
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description
-// @author       oralekin + odgrim + MESLewis
+// @author       oralekin + odgrim + MESLewis + T-Nicholls
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].getElementsByTagName("garlic-bread-share-container")[0].getElementsByTagName("garlic-bread-camera")[0].getElementsByTagName("garlic-bread-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/MESLewis/rplace/main/images/combined-overlay.png";
+            i.src = "https://raw.githubusercontent.com/T-Nicholls/rplace/main/images/combined-overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 500px;height: 500px;";
             console.log(i);
             return i;
